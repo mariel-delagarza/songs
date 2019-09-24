c9efb6e31d57df2c6c399a20e3166e564ff6f6e2
mariel-delagarza <mariel.code@gmail.com>
2019-09-24 16:22:26 -0400

    Create mapStateToProps function and use it in the export default statement

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SongDetail = () => {
+  console.log(props);
   return <div>Songdetail</div>;
 };
 
-export default SongDetail;
+const mapStateToProps = (state) => {
+  return { song: state.selectedSong }
+};
+
+export default connect(mapStateToProps)(SongDetail);
