13739e23ed83547b6150e37595471328ecfc72bf
mariel-delagarza <mariel.code@gmail.com>
2019-09-24 16:27:08 -0400

    Update function to account for instance in which song has not been chosen and prompt user to make a selection

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>
+  }
+  
   return <div>{song.title}</div>;
 };
 
